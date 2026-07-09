77f69ec105ea9a98125dd3d87296177abbfe2547
Lida <Ky6026010050@camtech.edu.kh>
2026-07-09 19:29:42 +0700

    feat:implement add product

---
+++ src/add_product.h
@@ -0,0 +1,6 @@
+#ifndef ADD_PRODUCT_H
+#define ADD_PRODUCT_H
+
+void addProduct();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,30 @@
+#include <stdio.h>
+#include "add_product.h"
+
+void displayMenu();
+
 int main() {
+    int choice;
+    do {
+        displayMenu();
+        scanf("%d", &choice);
+        while (getchar() != '\n');
+        switch (choice) {
+        case 1: addProduct(); break;
+        case 2: printf("Exiting... Goodbye!\n"); break;
+        default: printf("Invalid choice. Enter 1-2.\n");
+        }
+    } while (choice != 2);
     return 0;
+}
+
+void displayMenu() {
+    printf("\n====================================\n");
+    printf("  INVENTORY MANAGEMENT SYSTEM\n");
+    printf("         - CAMTECH -\n");
+    printf("====================================\n");
+    printf("1. Add Product\n");
+    printf("2. Exit\n");
+    printf("------------------------------------\n");
+    printf("Enter your choice: ");
 }
