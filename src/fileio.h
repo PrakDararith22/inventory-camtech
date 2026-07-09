fdb624b2697c0a1f290245c9e8da85996bedd1c5
Lida <Ky6026010050@camtech.edu.kh>
2026-07-09 19:35:54 +0700

    feat: implement appendLog

--- src/fileio.h
+++ src/fileio.h
@@ -9,5 +9,6 @@ int getAllProducts(Product *out, int max);
 int updateProductQty(const char *code, int newQty);
 int getProductCount();
 void generateCode(char *outCode);
+void appendLog(const char *action, const char *code, int qty);
 
 #endif
